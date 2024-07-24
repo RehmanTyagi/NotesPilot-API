54ad322ea85cbca1c5ecfe0f2aa3263e784424da
rehman tyagi <irontyagi4@gmail.com>
2024-07-25 01:49:12 +0530

    move assets folder into public

--- utils/generateAssetsImgURL.js
+++ utils/generateAssetsImgURL.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 function generateAssetsImgURL(filePath) {
-  const logoPath = path.join(__dirname, '..', 'assets', filePath);
+  const logoPath = `public/assets/${filePath}`
+  console.log(logoPath)
   const logo = fs.readFileSync(logoPath, 'base64')
   const logoSrc = `data:image/svg+xml;base64,${logo}`
   return logoSrc;
